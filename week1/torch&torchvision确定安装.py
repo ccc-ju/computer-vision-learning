c4f714e9e7dca0e09daa200de3ff69c7263f5060
ccc-ju <ccc-ju@outlook.com>
2020-07-01 17:39:57 +0800

    '第一次作业，图片识别'

---
+++ week1/torch&torchvision确定安装.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+@author:ccc-ju
+@software:PyCharm
+@time:2020/6/29 10:43
+'''
+
+import torch
+print(torch.__version__)
+print('gpu:', torch.cuda.is_available())
